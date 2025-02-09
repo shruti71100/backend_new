83eca5c9796afa849475982a201b3d03158cd7f5
shruti71100 <shrutikumari71100@gmail.com>
2025-02-09 16:03:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get("/listen",(req,res)=>{
+    res.send("i am a good girl")
+})
+app.get("/ayush",(req,res)=>{
+    res.send("<h1>hey i am a girl </h1>")
+})
+const port=process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
